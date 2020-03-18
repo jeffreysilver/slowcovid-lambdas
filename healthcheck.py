2d66006358d72587a95f1db65c41554a2db2f603
jeffreysilver <jeffreygsilver@gmail.com>
2020-03-18 14:42:08 -0400

    thin out serverless.yml and add events

--- healthcheck.py
+++ healthcheck.py
@@ -1,4 +1,10 @@
+from serverless_sdk import tag_event
+
+
 def healthcheck(event, context):
+    tag_event("healthcheck")
+
     return {
         "statusCode": 200,
+        "body": "up and at em"
     }
