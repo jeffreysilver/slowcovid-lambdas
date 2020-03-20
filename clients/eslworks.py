82311771c998466aff0b2a0e8e2c229feae0698a
jeffreysilver <jeffreygsilver@gmail.com>
2020-03-20 12:22:21 -0400

    use prod

--- clients/eslworks.py
+++ clients/eslworks.py
@@ -5,9 +5,10 @@ import os
 
 from serverless_sdk import tag_event
 
+BASE_URL = "https://eslworks-api-production.herokuapp.com/slowcovid"
 
 def register(payload):
-    resp = requests.post("https://eslworks-api-staging.herokuapp.com/slowcovid/register", auth=HTTPBasicAuth(os.environ.get("AUTH_USERNAME"), os.environ.get("AUTH_PASSWORD")), json=payload)
+    resp = requests.post(f"{BASE_URL}/register", auth=HTTPBasicAuth(os.environ.get("AUTH_USERNAME"), os.environ.get("AUTH_PASSWORD")), json=payload)
     tag_event("eslworks_register_status", resp.status_code)
     resp.raise_for_status()
     return resp
