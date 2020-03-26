3ea3c42021f14956b542b114194b6b2029dd3414
jeffreysilver <jeffreygsilver@gmail.com>
2020-03-26 17:58:58 -0400

    Add route inbound sms function

--- inbound_sms.py
+++ inbound_sms.py
@@ -5,17 +5,41 @@ from base64 import b64decode
 from clients import rds
 import os
 
-def store_inbound_sms(event, context):
-    tag_event('inbound_sms', 'raw_event', event)
+
+def _get_twilio_payload_from_record(record):
+    twilio_payload_bytes = b64decode(record["kinesis"]["data"])
+    return ast.literal_eval(twilio_payload_bytes.decode("UTF-8"))
+
+def _extract_twilio_payloads_from_kinesis_payload(kinesis_payload):
+    records = kinesis_payload["Records"]
+    return [
+        _get_twilio_payload_from_record(record)
+        for record in records
+    ]
+
+
+def store_message(event, context):
+    tag_event('store_message', 'raw_event', event)
     
     for record in event["Records"]:
         kinesis_payload = record["kinesis"]
-        base64_data = kinesis_payload["data"]
-        dict_bytes = b64decode(base64_data)
-        sms = ast.literal_eval(dict_bytes.decode("UTF-8"))
-        tag_event("inbound_sms", "message_payload", sms)
+        sms = _get_twilio_payload_from_record(record)
+        tag_event("store_message", "message_payload", sms)
         rds_response = rds.insert_message(sms["MessageSid"], sms["Body"], sms["From"], sms["To"], kinesis_payload["approximateArrivalTimestamp"], kinesis_payload["sequenceNumber"])
-        tag_event("inbound_sms", "rds_response", rds_response)
+        tag_event("store_message", "rds_response", rds_response)
+
+    return {
+        "statusCode": 200,
+    }
+
+def route_message(event, context):
+    tag_event("route_message", "raw_event", event)
+
+    twilio_payloads = _extract_twilio_payloads_from_kinesis_payload(event)
+    for sms in twilio_payloads:
+        tag_event("route_message", "message_payload", sms)
+
+        # route message to drill agent here
 
     return {
         "statusCode": 200,
