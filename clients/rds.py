72f6fa63cd518bb430f1f075d62c37bd9985de7c
jeffreysilver <jeffreygsilver@gmail.com>
2020-03-26 18:24:17 -0400

    format and lint

--- clients/rds.py
+++ clients/rds.py
@@ -1,7 +1,16 @@
 import os
 import boto3
 
-def insert_message(twilio_message_id, body, from_phone, to_phone, receive_status, approximate_arrival_timestamp, sequence_number):
+
+def insert_message(
+    twilio_message_id,
+    body,
+    from_phone,
+    to_phone,
+    receive_status,
+    approximate_arrival_timestamp,
+    sequence_number,
+):
     sql = """
         insert into
         message (
@@ -25,24 +34,27 @@ def insert_message(twilio_message_id, body, from_phone, to_phone, receive_status
     """
 
     sql_params = [
-        {'name': 'twilio_message_id', 'value': {'stringValue': twilio_message_id}},
-        {'name': 'body', 'value': {'stringValue': body}},
-        {'name': 'from_phone', 'value': {'stringValue': from_phone}},
-        {'name': 'to_phone', 'value': {'stringValue': to_phone}},
-        {'name': 'receive_status', 'value': {'stringValue': receive_status}},
-        {'name': 'approximate_arrival_timestamp', 'value': {'stringValue': str(approximate_arrival_timestamp)}},
-        {'name': 'sequence_number', 'value': {'stringValue': sequence_number}},
+        {"name": "twilio_message_id", "value": {"stringValue": twilio_message_id}},
+        {"name": "body", "value": {"stringValue": body}},
+        {"name": "from_phone", "value": {"stringValue": from_phone}},
+        {"name": "to_phone", "value": {"stringValue": to_phone}},
+        {"name": "receive_status", "value": {"stringValue": receive_status}},
+        {
+            "name": "approximate_arrival_timestamp",
+            "value": {"stringValue": str(approximate_arrival_timestamp)},
+        },
+        {"name": "sequence_number", "value": {"stringValue": sequence_number}},
     ]
 
     db_cluster_arn = os.environ.get("DB_CLUSTER_ARN")
     db_credentials_secret_arn = os.environ.get("DB_SECRET_ARN")
-    
-    rds_client = boto3.client('rds-data')
+
+    rds_client = boto3.client("rds-data")
 
     return rds_client.execute_statement(
         secretArn=db_credentials_secret_arn,
         database="postgres",
         resourceArn=db_cluster_arn,
         sql=sql,
-        parameters=sql_params
+        parameters=sql_params,
     )
--- healthcheck.py
+++ healthcheck.py
@@ -1,9 +1,4 @@
-import os
-from serverless_sdk import tag_event
-
-
 def healthcheck(event, context):
     return {
         "statusCode": 200,
-        "body": "https://i0.wp.com/media1.giphy.com/media/dkGhBWE3SyzXW/giphy.gif?zoom=2"
     }
