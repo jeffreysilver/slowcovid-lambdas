ee065f52b44ea93cc24b89170f120db82ea72078
jeffreysilver <jeffreygsilver@gmail.com>
2020-03-27 12:40:48 -0400

    restructure line breaks

--- clients/kinesis.py
+++ clients/kinesis.py
@@ -5,11 +5,10 @@ from serverless_sdk import tag_event
 
 
 def publish_outbound_message(to, body):
-    stage = os.environ.get("STAGE")
     kinesis = boto3.client("kinesis")
 
+    stage = os.environ.get("STAGE")
     payload = {"to": to, "body": body}
-
     response = kinesis.put_record(
         StreamName=f"outbound-sms-{stage}", Data=json.dumps(payload), PartitionKey=to,
     )
