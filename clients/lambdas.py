881990dac6ab2b4b20e9f820df1e8a2a33f3bb14
jeffreysilver <jeffreygsilver@gmail.com>
2020-03-28 13:17:55 -0400

    remove inbound x outbound flows and set up command stream

--- clients/lambdas.py
+++ clients/lambdas.py
@@ -3,12 +3,12 @@ import json
 import os
 
 
-def invoke_store_messages(payload):
+def invoke_send_message(payload):
     client = boto3.client("lambda")
     stage = os.environ["STAGE"]
 
     response = client.invoke(
-        FunctionName=f"slowcovid-{stage}-storeOutboundSMS",
+        FunctionName=f"slowcovid-{stage}-sendMessage",
         Payload=json.dumps(payload),
         InvocationType="Event",
     )
