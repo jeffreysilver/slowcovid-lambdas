93f055a71e48b003fcaa65dc7b02cd26ef640a9a
jeffreysilver <jeffreygsilver@gmail.com>
2020-03-30 16:48:30 -0400

    WIP - cloudformation and entries

--- clients/sqs.py
+++ clients/sqs.py
@@ -7,11 +7,16 @@ SQS = boto3.resource("sqs")
 
 
 def publish_outbound_sms_messages(payloads):
-    queue_name = f"outbound-sms-{os.getenv('STAGE')}"
+    # queue_name = f"outbound-sms-{os.getenv('STAGE')}"
     queue = SQS.get_queue_by_name(QueueName=queue_name)
-    entries = [
-        {"MessageBody": json.dumps(payload), "Id": str(uuid.uuid4())}
-        for payload in payloads
-    ]
+   
+    entries = []
+    for  payload in payloads:
+        idempotency_key = str(uuid.uuid4())
+        payload["idempotency_key"] = idempotency_key
+        entries.append({
+            "Id": idempotency_key,
+            "MessageBody": json.dumps(payload),
+        })
 
     return queue.send_messages(Entries=entries)
