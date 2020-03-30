65b7da3fbe6b41f886f69445fafed7bfb12b4b49
jeffreysilver <jeffreygsilver@gmail.com>
2020-03-30 18:36:50 -0400

    black

--- clients/dynamo.py
+++ clients/dynamo.py
@@ -5,15 +5,19 @@ import time
 
 DYNAMO = boto3.client("dynamodb")
 
-OUTBOUND_SMS_IDEMPOTENCY_KEY_TABLE_NAME = f"outbound-sms-idempotency-{os.getenv('STAGE')}"
+OUTBOUND_SMS_IDEMPOTENCY_KEY_TABLE_NAME = (
+    f"outbound-sms-idempotency-{os.getenv('STAGE')}"
+)
+
 
 def persist_outbound_sms_idempotency_key(idempotency_key):
-    expire_at = time.time() + 60 * 60 * 24 # clean up idempotency keys after one day
+    expire_at = time.time() + 60 * 60 * 24  # clean up idempotency keys after one day
     return DYNAMO.put_item(
-        TableName=OUTBOUND_SMS_IDEMPOTENCY_KEY_TABLE_NAME, Item={
+        TableName=OUTBOUND_SMS_IDEMPOTENCY_KEY_TABLE_NAME,
+        Item={
             "idempotency_key": {"S": idempotency_key},
             "expire_at": {"N": str(expire_at)},
-        }
+        },
     )
 
 
