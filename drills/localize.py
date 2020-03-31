c35943d9283211dc5cf81c9c8d2ff28ab0732c0e
Philip Sarin <psarin@managedbyq.com>
2020-03-31 12:34:24 -0400

    don't allow unsupported languages

--- drills/localize.py
+++ drills/localize.py
@@ -10,8 +10,13 @@ CACHE = None
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
 
+SUPPORTED_LANGUAGES = {"en", "es", "fr", "pt", "zh"}
+
+
 def localize(message: str, lang: str, **kwargs) -> str:
     lang = lang or "en"
+    if lang not in SUPPORTED_LANGUAGES:
+        lang = "en"
     template = Template(message)
     result = template.render(**localizations_for(lang))
     if kwargs:
--- drills/test_localize.py
+++ drills/test_localize.py
@@ -5,7 +5,7 @@ from . import localize
 
 class TestLocalize(unittest.TestCase):
     def test_localization(self):
-        en = localize.localizations_for("en")
-        fr = localize.localizations_for("fr")
-        self.assertEqual("Today's drill is:", en["drill_intro_2"])
-        self.assertEqual("La formation d'aujourd'hui est: \n", fr["drill_intro_2"])
+        self.assertEqual("Today's drill is:", localize.localize("{{drill_intro_2}}", "en"))
+        self.assertEqual("La formation d'aujourd'hui est: \n",
+                         localize.localize("{{drill_intro_2}}", "fr"))
+        self.assertEqual("Today's drill is:", localize.localize("{{drill_intro_2}}", "xx"))
