ed03c7d9c9e750f9918a8dd03671d115cfe5dd0d
jeffreysilver <jeffreygsilver@gmail.com>
2020-03-31 15:25:48 -0400

    br

--- __tests__/stopcovid/test_event_distributor.py
+++ __tests__/stopcovid/test_event_distributor.py
@@ -9,14 +9,12 @@ class TestHandleCommand(unittest.TestCase):
     @patch("stopcovid.clients.sqs.SQS")
     def test_distribute_events(self, sqs_mock):
         queue = MagicMock(name="queue")
-
         send_messages = MagicMock()
         queue.send_messages = send_messages
         sqs_mock.get_queue_by_name = MagicMock(return_value=queue)
 
         with open("sample_events/distribute_event.json") as f:
             mock_event = json.load(f)
-
         distribute_dialog_events(mock_event, None)
 
         send_messages.assert_called_once()
@@ -24,9 +22,7 @@ class TestHandleCommand(unittest.TestCase):
         _, *kwargs = call
 
         entries = kwargs[1]["Entries"]
-
         self.assertEqual(len(entries), 3)
-
         self.assertEqual(entries[0]["Id"], "5e539115-07ac-4a53-809b-2280b2ce734b")
         message_body = json.loads(entries[0]["MessageBody"])
         self.assertEqual(message_body["idempotency_key"], "5e539115-07ac-4a53-809b-2280b2ce734b")
