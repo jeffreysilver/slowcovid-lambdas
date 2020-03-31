d79d1a5da0472e6c3b9500e32e05d453ee9bc897
jeffreysilver <jeffreygsilver@gmail.com>
2020-03-31 17:29:10 -0400

    remove unused utils

--- stopcovid/outbound_sms.py
+++ stopcovid/outbound_sms.py
@@ -2,7 +2,6 @@ from serverless_sdk import tag_event
 import json
 
 from stopcovid.clients import twilio, kinesis, dynamo
-from stopcovid.utils.sqs import get_payloads_from_sqs_event
 
 
 def send_message(event, context):
