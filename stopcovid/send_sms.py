9443a5e9a7ffc33ccab1adffbe78dc29f28277b8
jeffreysilver <jeffreygsilver@gmail.com>
2020-04-01 17:57:15 -0400

    Tag event for failed kinesis publish

--- stopcovid/send_sms.py
+++ stopcovid/send_sms.py
@@ -1,5 +1,7 @@
 import json
 from time import sleep
+from serverless_sdk import tag_event
+
 from stopcovid.clients import twilio, kinesis
 
 
@@ -9,11 +11,30 @@ def _get_delay_seconds(record):
 
 def _send_message_to_twilio(record):
     message = json.loads(record["body"])
-    sleep(_get_delay_seconds(record))
+    delay_seconds = _get_delay_seconds(record)
+    if delay_seconds:
+        sleep(delay_seconds)
     return twilio.send_message(message["To"], message["Body"])
 
 
 def send_sms(event, context):
     twilio_responses = [_send_message_to_twilio(record) for record in event["Records"]]
-    kinesis.publish_log_outbound_sms(twilio_responses)
+    try:
+        kinesis.publish_log_outbound_sms(twilio_responses)
+    except Exception:
+        tag_event(
+            "send_sms",
+            "failed_to_write_to_kinesis_log",
+            [
+                {
+                    "twilio_message_id": response.sid,
+                    "to": response.to,
+                    "body": response.body,
+                    "status": response.status,
+                    "error_code": response.error_code,
+                    "error_message": response.error_message,
+                }
+                for response in twilio_responses
+            ],
+        )
     return {"statusCode": 200}
