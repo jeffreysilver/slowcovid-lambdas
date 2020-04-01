330ff8bf2b1226c92dd0c8a125839b62b069900a
jeffreysilver <jeffreygsilver@gmail.com>
2020-04-01 15:35:58 -0400

    delay sending messages

--- stopcovid/event_distributor/outbound_sms.py
+++ stopcovid/event_distributor/outbound_sms.py
@@ -24,31 +24,38 @@ USER_VALIDATION_FAILED_COPY = (
 # We should template these for localization
 CORRECT_ANSWER_COPY = "Correct!"
 
+DELAY_BETWEEN_MESSAGE_IN_SECONDS = 3
+
 
 @dataclass
 class OutboundSMS:
     event_id: str
     phone_number: str
     body: str
+    delay_seconds: int
 
 
 def get_localized_messages(
-    dialog_event: DialogEvent, messages: List[str], **kwargs
+    dialog_event: DialogEvent, messages: List[str], with_initial_pause: bool = False, **kwargs
 ) -> List[OutboundSMS]:
     language = dialog_event.user_profile.language
+
+    initial_pause_seconds = DELAY_BETWEEN_MESSAGE_IN_SECONDS if with_initial_pause else 0
+
     return [
         OutboundSMS(
             event_id=dialog_event.event_id,
             phone_number=dialog_event.phone_number,
             body=localize(message, language, **kwargs),
+            delay_seconds=initial_pause_seconds + (i * DELAY_BETWEEN_MESSAGE_IN_SECONDS),
         )
-        for message in messages
+        for i, message in enumerate(messages)
     ]
 
 
 def get_messages_for_command(event: DialogEvent):  # noqa: C901
     if isinstance(event, AdvancedToNextPrompt):
-        return get_localized_messages(event, event.prompt.messages)
+        return get_localized_messages(event, event.prompt.messages, with_initial_pause=True)
 
     elif isinstance(event, FailedPrompt):
         if not event.abandoned:
