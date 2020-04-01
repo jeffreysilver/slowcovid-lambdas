91dd07f30ced296ac09d2a7028b6ff69a7cfe399
Philip Sarin <psarin@managedbyq.com>
2020-04-01 10:56:07 -0400

    separate answer localization testing from the core of the dialog agent testing

--- __tests__/stopcovid/drills/test_drills.py
+++ __tests__/stopcovid/drills/test_drills.py
@@ -1,6 +1,7 @@
 import json
 import os
 import unittest
+from unittest.mock import patch
 
 from stopcovid.drills import drills
 
@@ -22,3 +23,26 @@ class TestDrillFileIntegrity(unittest.TestCase):
             for slug, drill_dict in drills_dict.items():
                 self.assertEqual(slug, drill_dict["slug"])
                 drills.get_drill(slug)  # make sure it doesn't blow up
+
+
+class TestPrompt(unittest.TestCase):
+    def test_should_advance_ignore(self):
+        prompt = drills.Prompt(slug="test-prompt", messages=["{{msg1}}"])
+        self.assertTrue(prompt.should_advance_with_answer("any answer", "en"))
+
+    def test_should_advance_store(self):
+        prompt = drills.Prompt(
+            slug="test-prompt", messages=["{{msg1}}"], response_user_profile_key="self_rating_7"
+        )
+        self.assertTrue(prompt.should_advance_with_answer("any answer", "en"))
+
+    def test_should_advance_graded(self):
+        prompt = drills.Prompt(
+            slug="test-prompt", messages=["{{msg1}}"], correct_response="{{resp1}}"
+        )
+        with patch("stopcovid.drills.drills.localize") as localize_mock:
+            localize_mock.return_value = "my response"
+            self.assertFalse(
+                prompt.should_advance_with_answer("something completely different", "en")
+            )
+            self.assertTrue(prompt.should_advance_with_answer("my response", "en"))
