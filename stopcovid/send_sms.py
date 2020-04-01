b81d6f92305499a20f83fd8a76fc906eb92da5f6
jeffreysilver <jeffreygsilver@gmail.com>
2020-04-01 17:45:18 -0400

    Remove if

--- stopcovid/send_sms.py
+++ stopcovid/send_sms.py
@@ -4,7 +4,7 @@ from stopcovid.clients import twilio, kinesis
 
 
 def _get_delay_seconds(record):
-    return int(record["MessageAttributes"].get("delay_seconds", {}).get("stringValue", "0"))
+    return int(record["messageAttributes"].get("delay_seconds", {}).get("stringValue", "0"))
 
 
 def _send_message_to_twilio(record):
@@ -15,8 +15,5 @@ def _send_message_to_twilio(record):
 
 def send_sms(event, context):
     twilio_responses = [_send_message_to_twilio(record) for record in event["Records"]]
-
-    if twilio_responses:
-        kinesis.publish_log_outbound_sms(twilio_responses)
-
+    kinesis.publish_log_outbound_sms(twilio_responses)
     return {"statusCode": 200}
