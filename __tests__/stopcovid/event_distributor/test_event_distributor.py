fc6328189158902e5474bc86b3608853192dba2f
jeffreysilver <jeffreygsilver@gmail.com>
2020-04-01 21:12:37 -0400

    tweaks from end to end test (#47)

--- __tests__/stopcovid/event_distributor/test_event_distributor.py
+++ __tests__/stopcovid/event_distributor/test_event_distributor.py
@@ -29,7 +29,7 @@ class TestHandleCommand(unittest.TestCase):
 
         entries = kwargs[1]["Entries"]
         self.assertEqual(len(entries), 3)
-        self.assertEqual(entries[0]["Id"], "5e539115-07ac-4a53-809b-2280b2ce734b")
+        self.assertEqual(entries[0]["Id"], "5e539115-07ac-4a53-809b-2280b2ce734b-0")
         message_body = json.loads(entries[0]["MessageBody"])
         self.assertEqual(message_body["To"], "+14802865415")
         self.assertIsInstance(message_body["Body"], str)
