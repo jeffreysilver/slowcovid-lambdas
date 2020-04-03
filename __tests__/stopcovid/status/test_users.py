54e78c56f8604dd0e46d76ad564f62619b6ff98a
Philip Sarin <phil.sarin@gmail.com>
2020-04-02 21:57:19 -0400

    update drill progress for each user

--- __tests__/stopcovid/status/test_users.py
+++ __tests__/stopcovid/status/test_users.py
@@ -1,24 +1,141 @@
 import unittest
+import datetime
+from copy import copy
 
+from stopcovid.dialog.dialog import (
+    UserValidated,
+    DrillStarted,
+    DrillCompleted,
+    CompletedPrompt,
+    FailedPrompt,
+)
+from stopcovid.dialog.registration import CodeValidationPayload
 from stopcovid.dialog.types import UserProfile
+from stopcovid.drills.drills import Prompt, Drill
 from stopcovid.status.db import get_test_sqlalchemy_engine
-from stopcovid.status.users import UserRepository
+from stopcovid.status.users import UserRepository, ALL_DRILL_SLUGS
 
 
 class TestUsers(unittest.TestCase):
     def setUp(self):
         self.repo = UserRepository(get_test_sqlalchemy_engine)
         self.repo.drop_and_recreate_tables_testing_only()
+        self.phone_number = "123456789"
+        self.prompt = Prompt(slug="first", messages=[])
+        self.drill = Drill(slug="slug", name="name", prompts=[self.prompt])
 
     def test_create_or_update_user(self):
         profile = UserProfile(True, account_info={"foo": "bar"})
-        user_id = self.repo.create_or_update_user("123456789", profile)
+        user_id = self.repo.create_or_update_user(self.phone_number, profile)
         user = self.repo.get_user(user_id)
         self.assertEqual(user_id, user.user_id)
         self.assertEqual({"foo": "bar"}, user.account_info)
         self.assertIsNone(user.last_interacted_time)
 
         profile.account_info["one"] = "two"
-        self.repo.create_or_update_user("123456789", profile)
+        self.repo.create_or_update_user(self.phone_number, profile)
         user = self.repo.get_user(user_id)
         self.assertEqual({"foo": "bar", "one": "two"}, user.account_info)
+
+    def test_user_revalidated(self):
+        user_id = self.repo.create_or_update_user(self.phone_number, UserProfile(True))
+        time = datetime.datetime.now(datetime.timezone.utc)
+        for slug in ALL_DRILL_SLUGS:
+            drill = copy(self.drill)
+            drill.slug = slug
+            event = DrillStarted(
+                phone_number=self.phone_number,
+                user_profile=UserProfile(True),
+                drill=self.drill,
+                first_prompt=self.prompt,
+            )
+            event2 = DrillCompleted(
+                phone_number=self.phone_number,
+                user_profile=UserProfile(True),
+                drill_instance_id=event.drill_instance_id,
+            )
+            self.repo._mark_drill_started(user_id, event)
+            self.repo._mark_drill_completed(user_id, event2)
+            drill_status = self.repo.get_drill_status(user_id, slug)
+            self.assertIsNotNone(drill_status.started_time)
+            self.assertIsNotNone(drill_status.completed_time)
+        self.repo.update_user_progress(
+            user_id,
+            UserValidated(
+                phone_number=self.phone_number,
+                user_profile=UserProfile(True),
+                code_validation_payload=CodeValidationPayload(valid=True),
+            ),
+        )
+        for slug in ALL_DRILL_SLUGS:
+            drill_status = self.repo.get_drill_status(user_id, slug)
+            self.assertIsNone(drill_status.started_time)
+            self.assertIsNone(drill_status.completed_time)
+
+    def test_drill_started_and_completed(self):
+        user_id = self.repo.create_or_update_user(self.phone_number, UserProfile(True))
+        event = DrillStarted(
+            phone_number=self.phone_number,
+            user_profile=UserProfile(True),
+            drill=self.drill,
+            first_prompt=self.prompt,
+        )
+        self.repo.update_user_progress(user_id, event)
+        drill_status = self.repo.get_drill_status(user_id, self.prompt.slug)
+        self.assertEqual(event.created_time, drill_status.started_time)
+        self.assertIsNone(drill_status.completed_time)
+
+        event2 = DrillCompleted(
+            phone_number=self.phone_number,
+            user_profile=UserProfile(True),
+            drill_instance_id=event.drill_instance_id,
+        )
+        self.repo.update_user_progress(user_id, event2)
+        drill_status = self.repo.get_drill_status(user_id, self.prompt.slug)
+        self.assertEqual(event.created_time, drill_status.started_time)
+        self.assertEqual(event2.created_time, drill_status.completed_time)
+
+    def test_completed_prompt(self):
+        user_id = self.repo.create_or_update_user(self.phone_number, UserProfile(True))
+        event = DrillStarted(
+            phone_number=self.phone_number,
+            user_profile=UserProfile(True),
+            drill=self.drill,
+            first_prompt=self.prompt,
+        )
+        self.repo.update_user_progress(user_id, event)
+        user = self.repo.get_user(user_id)
+        self.assertIsNone(user.last_interacted_time)
+        event2 = CompletedPrompt(
+            phone_number=self.phone_number,
+            user_profile=UserProfile(True),
+            prompt=self.prompt,
+            drill_instance_id=event.drill_instance_id,
+            response="go",
+        )
+        self.repo.update_user_progress(user_id, event2)
+        user = self.repo.get_user(user_id)
+        self.assertEqual(event2.created_time, user.last_interacted_time)
+
+    def test_failed_prompt(self):
+        user_id = self.repo.create_or_update_user(self.phone_number, UserProfile(True))
+        event = DrillStarted(
+            phone_number=self.phone_number,
+            user_profile=UserProfile(True),
+            drill=self.drill,
+            first_prompt=self.prompt,
+        )
+        self.repo.update_user_progress(user_id, event)
+        user = self.repo.get_user(user_id)
+        self.assertIsNone(user.last_interacted_time)
+        event2 = FailedPrompt(
+            phone_number=self.phone_number,
+            user_profile=UserProfile(True),
+            prompt=self.prompt,
+            drill_instance_id=event.drill_instance_id,
+            response="go",
+            abandoned=True,
+        )
+        self.repo.update_user_progress(user_id, event2)
+        user = self.repo.get_user(user_id)
+        self.assertEqual(event2.created_time, user.last_interacted_time)
--- stopcovid/status/users.py
+++ stopcovid/status/users.py
@@ -1,7 +1,8 @@
 import datetime
+import logging
 import uuid
 from dataclasses import dataclass, field
-from typing import Dict, Any, Optional
+from typing import Dict, Any, Optional, Union
 
 from sqlalchemy import (
     Table,
@@ -19,7 +20,18 @@ from sqlalchemy.dialects.postgresql import UUID, JSONB
 from sqlalchemy.exc import DatabaseError
 
 from . import db
-from stopcovid.dialog.types import UserProfile
+from stopcovid.dialog.types import UserProfile, DialogEvent
+from .drill_instances import DrillInstanceRepository
+from ..dialog.dialog import (
+    UserValidated,
+    DrillStarted,
+    DrillCompleted,
+    CompletedPrompt,
+    FailedPrompt,
+    AdvancedToNextPrompt,
+    ReminderTriggered,
+    UserValidationFailed,
+)
 
 ALL_DRILL_SLUGS = [
     "01-basics",
@@ -54,10 +66,12 @@ drill_statuses = Table(
     metadata,
     Column("id", UUID, primary_key=True),
     Column("user_id", UUID, ForeignKey("users.user_id"), nullable=False),
+    Column("drill_instance_id", UUID, nullable=True, index=True),
     Column("drill_slug", String, nullable=False),
     Column("place_in_sequence", Integer, nullable=False),
-    Column("started_time", DateTime),
-    Column("completed_time", DateTime),
+    Column("started_time", DateTime(timezone=True)),
+    Column("reminder_sent_time", DateTime(timezone=True)),
+    Column("completed_time", DateTime(timezone=True)),
 )
 
 
@@ -76,12 +90,79 @@ class PhoneNumber:
     id: UUID = field(default_factory=uuid.uuid4)
 
 
+@dataclass
+class DrillStatus:
+    id: uuid.UUID
+    user_id: uuid.UUID
+    drill_instance_id: uuid.UUID
+    drill_slug: str
+    place_in_sequence: int
+    started_time: datetime.datetime
+    reminder_sent_time: datetime.datetime
+    completed_time: datetime.datetime
+
+
 class UserRepository:
     def __init__(self, engine_factory=db.get_sqlalchemy_engine):
         self.engine_factory = engine_factory
         self.engine = engine_factory()
 
-    def create_or_update_user(self, phone_number: str, profile: UserProfile) -> UUID:
+    def get_user(self, user_id: uuid.UUID) -> Optional[User]:
+        result = self.engine.execute(
+            select([users]).where(users.c.user_id == func.uuid(str(user_id)))
+        )
+        row = result.fetchone()
+        if row is None:
+            return None
+        return User(
+            user_id=uuid.UUID(row["user_id"]),
+            account_info=row["account_info"],
+            last_interacted_time=row["last_interacted_time"],
+        )
+
+    def get_drill_status(self, user_id: uuid.UUID, drill_slug: str) -> Optional[DrillStatus]:
+        result = self.engine.execute(
+            select([drill_statuses]).where(
+                drill_statuses.c.user_id == func.uuid(str(user_id))
+                and drill_statuses.c.drill_slug == drill_slug
+            )
+        )
+        row = result.fetchone()
+        if row is None:
+            return None
+        return DrillStatus(
+            id=uuid.UUID(row["id"]),
+            user_id=uuid.UUID(row["user_id"]),
+            drill_instance_id=uuid.UUID(row["drill_instance_id"]),
+            drill_slug=row["drill_slug"],
+            place_in_sequence=row["place_in_sequence"],
+            started_time=row["started_time"],
+            reminder_sent_time=row["reminder_sent_time"],
+            completed_time=row["completed_time"],
+        )
+
+    def update_user_progress(self, user_id: uuid.UUID, event: DialogEvent):
+        if isinstance(event, UserValidated):
+            self._reset_drill_statuses(user_id)
+        elif isinstance(event, DrillStarted):
+            self._mark_drill_started(user_id, event)
+        elif isinstance(event, DrillCompleted):
+            self._mark_drill_completed(user_id, event)
+        elif isinstance(event, CompletedPrompt):
+            self._mark_interaction_time(user_id, event)
+        elif isinstance(event, FailedPrompt):
+            self._mark_interaction_time(user_id, event)
+        elif (
+            isinstance(event, AdvancedToNextPrompt)
+            or isinstance(event, ReminderTriggered)
+            or isinstance(event, UserValidationFailed)
+            or isinstance(event, AdvancedToNextPrompt)
+        ):
+            logging.info(f"Ignoring event of type {event.event_type}")
+        else:
+            raise ValueError(f"Unknown event type {event.event_type}")
+
+    def create_or_update_user(self, phone_number: str, profile: UserProfile) -> uuid.UUID:
 
         with self.engine.connect() as connection:
             with connection.begin():
@@ -125,17 +206,38 @@ class UserRepository:
                 )
                 return phone_number_record.user_id
 
-    def get_user(self, user_id: UUID) -> Optional[User]:
-        result = self.engine.execute(
-            select([users]).where(users.c.user_id == func.uuid(str(user_id)))
+    def _reset_drill_statuses(self, user_id: uuid.UUID):
+        self.engine.execute(
+            drill_statuses.update()
+            .where(drill_statuses.c.user_id == func.uuid(str(user_id)))
+            .values(started_time=None, reminder_sent_time=None, completed_time=None)
         )
-        row = result.fetchone()
-        if row is None:
-            return None
-        return User(
-            user_id=uuid.UUID(row["user_id"]),
-            account_info=row["account_info"],
-            last_interacted_time=row["last_interacted_time"],
+
+    def _mark_drill_started(self, user_id: uuid.UUID, event: DrillStarted):
+        self.engine.execute(
+            drill_statuses.update()
+            .where(
+                drill_statuses.c.user_id == func.uuid(str(user_id))
+                and drill_statuses.c.drill_slug == event.drill.slug
+            )
+            .values(started_time=event.created_time, drill_instance_id=str(event.drill_instance_id))
+        )
+
+    def _mark_drill_completed(self, user_id: uuid.UUID, event: DrillCompleted):
+        self.engine.execute(
+            drill_statuses.update()
+            .where(
+                drill_statuses.c.user_id == func.uuid(str(user_id))
+                and drill_statuses.c.drill_instance_id == func.uuid(str(event.drill_instance_id))
+            )
+            .values(completed_time=event.created_time)
+        )
+
+    def _mark_interaction_time(self, user_id, event: Union[CompletedPrompt, FailedPrompt]):
+        self.engine.execute(
+            users.update()
+            .where(users.c.user_id == func.uuid(str(user_id)))
+            .values(last_interacted_time=event.created_time)
         )
 
     def drop_and_recreate_tables_testing_only(self):
