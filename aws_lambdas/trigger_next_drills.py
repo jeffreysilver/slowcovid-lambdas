52bde9af30563bfe721664bf698c4756ed8e4887
Philip Sarin <phil.sarin@gmail.com>
2020-04-04 08:38:34 -0400

    put initiation in the right place

--- aws_lambdas/trigger_next_drills.py
+++ aws_lambdas/trigger_next_drills.py
@@ -1,41 +1,7 @@
-import json
-import os
-from typing import Iterator
-
-import boto3
-
-from stopcovid.drills.drills import get_drill
-from stopcovid.status.users import UserRepository, DrillProgress
-
-INACTIVITY_THRESHOLD_MINUTES = 721
+from stopcovid.status.initiation import trigger_next_drills
 
 
 def handler(event, context):
-    repo = UserRepository()
-    _publish_start_drill_commands(
-        repo.get_progress_for_users_who_need_drills(INACTIVITY_THRESHOLD_MINUTES)
-    )
+    trigger_next_drills()
 
     return {"statusCode": 200}
-
-
-def _publish_start_drill_commands(drill_progress_statuses: Iterator[DrillProgress]):
-    kinesis = boto3.client("kinesis")
-    records = [
-        {
-            "Data": json.dumps(
-                {
-                    "type": "START_DRILL",
-                    "payload": {
-                        "phone_number": drill_progress.phone_number,
-                        "drill": get_drill(drill_progress.next_drill_slug_to_trigger()).to_dict(),
-                    },
-                }
-            ),
-            "PartitionKey": drill_progress.phone_number,
-        }
-        for drill_progress in drill_progress_statuses
-    ]
-    if records:
-        stage = os.environ.get("STAGE")
-        kinesis.put_records(Records=records, StreamName=f"command-stream-{stage}")
