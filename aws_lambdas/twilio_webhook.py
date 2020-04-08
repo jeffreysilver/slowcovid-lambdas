60d463dc8e77a4d2219b48843a9348d821678490
Philip Sarin <phil.sarin@gmail.com>
2020-04-08 13:46:04 -0400

    clean things up

--- aws_lambdas/twilio_webhook.py
+++ aws_lambdas/twilio_webhook.py
@@ -1,6 +1,7 @@
 import json
 import logging
 import os
+from typing import Any, Dict
 from urllib.parse import unquote_plus
 
 import boto3
@@ -16,23 +17,41 @@ def handler(event, context):
     kinesis = boto3.client("kinesis")
     stage = os.getenv("STAGE")
 
-    validator = RequestValidator(os.getenv("TWILIO_AUTH_TOKEN"))
-    url = f"https://{event['headers']['Host']}/{stage}{event['path']}"
-    form = {
-        split_pair[0]: unquote_plus(split_pair[1])
-        for split_pair in [kvpair.split("=") for kvpair in event["body"].split("&")]
-    }
-    signature = event["headers"].get("X-Twilio-Signature")
-    if not validator.validate(url, form, signature):
+    form = extract_form(event)
+    if not is_signature_valid(event, form, stage):
         logging.warning("signature validation failed")
         return {"statusCode": 403}
     if "MessageStatus" in form:
         kinesis.put_record(
             Data=json.dumps({"type": "STATUS_UPDATE", "payload": form}),
-            PartitionKey=event["To"],
+            PartitionKey=form["To"],
             StreamName=f"message-log-{stage}",
         )
         return
 
     CommandPublisher().publish_process_sms_command(form["From"], form["Body"])
+    kinesis.put_record(
+        Data=json.dumps({"type": "INBOUND_SMS", "payload": form}),
+        PartitionKey=form["To"],
+        StreamName=f"message-log-{stage}",
+    )
+
     return {"statusCode": 200}
+
+
+def extract_form(event):
+    # We're getting an x-www-form-url-encoded string and we need to translate it into a dict.
+    # We aren't using urllib.parse.parse_qs because it gives a slightly different answer, resulting
+    # in failed signature validation.
+
+    return {
+        split_pair[0]: unquote_plus(split_pair[1])
+        for split_pair in [kvpair.split("=") for kvpair in event["body"].split("&")]
+    }
+
+
+def is_signature_valid(event: Dict[str, Any], form: Dict[str, Any], stage: str) -> bool:
+    validator = RequestValidator(os.getenv("TWILIO_AUTH_TOKEN"))
+    url = f"https://{event['headers']['Host']}/{stage}{event['path']}"
+    signature = event["headers"].get("X-Twilio-Signature")
+    return validator.validate(url, form, signature)
