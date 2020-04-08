8cf213a5fb6bbb511fcd98bce6fa877ad3c173c8
Philip Sarin <phil.sarin@gmail.com>
2020-04-08 13:57:11 -0400

    get rid of old handleInboundSMS handler

--- aws_lambdas/handle_inbound_sms.py
+++
@@ -1,9 +0,0 @@
-from stopcovid.dialog.command_stream.publish import CommandPublisher
-from stopcovid.utils.logging import configure_logging
-
-configure_logging()
-
-
-def handler(event, context):
-    CommandPublisher().publish_process_sms_command(event["From"], event["Body"])
-    return {"statusCode": 200}
