a439aaca9d946aa3440b1e16c9052efabeb4f763
Philip Sarin <phil.sarin@gmail.com>
2020-04-09 16:03:16 -0400

    copy fix

--- stopcovid/utils/verify_deploy_stage.py
+++ stopcovid/utils/verify_deploy_stage.py
@@ -6,6 +6,6 @@ def verify_deploy_stage():
     deploy_stage = os.getenv("DEPLOY_STAGE")
     if stage != deploy_stage:
         raise EnvironmentError(
-            f"There is a mismatch between your stage and environment variables. "
+            f"There is a mismatch between the stage and environment variables. "
             f"Exiting. (STAGE={stage}, DEPLOY_STAGE={deploy_stage})"
         )
