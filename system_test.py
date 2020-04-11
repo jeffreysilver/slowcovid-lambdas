b5babbdbaf8f73769afe830cc778b2c3a3100264
jeffreysilver <jeffreygsilver@gmail.com>
2020-04-11 17:52:51 -0400

    simplify system test

--- system_test.py
+++ system_test.py
@@ -20,16 +20,13 @@ class SystemTest:
         self.twilio_client = Client(
             os.environ["TWILIO_ACCOUNT_SID"], os.environ["TWILIO_AUTH_TOKEN"]
         )
-        self.drill_complete = False
+        self.test_complete = False
 
     def respond(self, body):
         logging.info(f"Responding: {body}")
         self.twilio_client.messages.create(
             to=STOPCOVID_DEV_PHONE_NUMBER, from_=SYSTEM_TEST_PHONE_NUMBER, body=body
         )
-
-    def kick_off_drill(self):
-        self.respond("stopcovid")
 
     def _handle_response(self, text):
         lowered_text = text.lower()
@@ -38,30 +35,15 @@ class SystemTest:
             self.respond("en")
 
         if "text me the word go" in lowered_text:
-            self.respond("go")
+            self.test_complete = True
 
-        if "on a scale of 1-10" in lowered_text:
-            self.respond("7")
-
-        if "what's your full name" in lowered_text:
-            self.respond("mr robot")
-
-        if "PRACTICE" in text:
-            self.respond("a")
-
-        if "try again" in lowered_text:
-            self.respond("b")
-
-        if "drill is complete" in lowered_text:
-            self.drill_complete = True
-
-    def proceed_through_drill(self):
+    def execute(self):
+        self.respond("stopcovid")
         idle_count = 0
-        while not self.drill_complete:
+        while not self.test_complete:
             if idle_count > 5:
                 raise RuntimeError("System is not responding in time.")
 
-            logging.info(".")
             messages = self.queue.receive_messages(WaitTimeSeconds=1)
             if messages:
                 idle_count = 0
@@ -75,10 +57,6 @@ class SystemTest:
                 idle_count += 1
 
             sleep(1)
-
-    def execute(self):
-        self.kick_off_drill()
-        self.proceed_through_drill()
         logging.info("System test complete")
 
 
