4e079126dce020039e9e5e129e2c11fab91f90a4
Philip Sarin <phil.sarin@gmail.com>
2020-04-12 11:27:32 -0400

    stop double logging everything

--- stopcovid/utils/logging.py
+++ stopcovid/utils/logging.py
@@ -13,13 +13,3 @@ def configure_logging():
 
     root = logging.getLogger()
     root.setLevel(logging.INFO)
-
-    handler = logging.StreamHandler(sys.stdout)
-    handler.setLevel(logging.INFO)
-    formatter = logging.Formatter(
-        "%(levelname)-8s %(asctime)s "
-        "%(filename)s:%(lineno)s - %(name)s - %(funcName)s: "
-        "%(message)s"
-    )
-    handler.setFormatter(formatter)
-    root.addHandler(handler)
